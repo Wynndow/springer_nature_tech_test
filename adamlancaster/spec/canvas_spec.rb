d4563329b5984c7655245358c9dcea7649a8a608
Adam Lancaster <lancaster28@hotmail.com>
2016-03-18 11:21:31 +0000

    Setup

--- adamlancaster/spec/canvas_spec.rb
+++ adamlancaster/spec/canvas_spec.rb
@@ -1,8 +1,14 @@
 require 'canvas.rb'
 
 describe Canvas do
+  subject(:canvas){described_class.new}
   
   it "prints a canvas" do
+    expect(canvas.print(2,2)).to eq(
+      "----\n"+
+      "|  |\n"+
+      "|  |\n"+
+      "----")
 
   end
 
