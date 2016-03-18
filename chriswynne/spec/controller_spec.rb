64558da3d8a9444e86bd304e8c73eca3da84e610
Chris Wynne <chris.wynndow@gmail.com>
2016-03-18 15:34:59 +0000

    Adds parse string function WIP

--- chriswynne/spec/controller_spec.rb
+++ chriswynne/spec/controller_spec.rb
@@ -5,16 +5,39 @@ describe Controller do
   let(:ui) { double("ui") }
   let(:controller) {described_class.new(ui)}
 
-  it 'can get quit from it\'s loop' do
-    allow(ui).to receive(:get_command).and_return("Q")
-    expect(ui).to receive(:get_command).once
-    controller.run
+  describe '#get_command' do
+
+    it 'can get quit from it\'s loop' do
+      allow(ui).to receive(:get_command).and_return("Q")
+      expect(ui).to receive(:get_command).once
+      controller.run
+    end
+
+    it 'continues the loop intil quit is supplied' do
+      allow(ui).to receive(:get_command).and_return("C 20 4", "Q")
+      expect(ui).to receive(:get_command).twice
+      controller.run
+    end
+
+    it 'creates new canvas from C command' do
+      allow(ui).to receive(:get_command).and_return("C 1 4", "Q")
+      expect(ui).to receive(:output).with(
+      "------ \n"+
+      "|    | \n"+
+      "------ \n")
+      controller.run
+    end
+
   end
 
-  it 'continues the loop intil quit is supplied' do
-    allow(ui).to receive(:get_command).and_return("C 20 4", "Q")
-    expect(ui).to receive(:get_command).twice
-    controller.run
+  describe '#parse_string' do
+    it 'can return the canvas string' do
+      expect(controller.parse_string("C 4 1")).to eq(
+      "------\n"+
+      "|    |\n"+
+      "------\n")
+    end
+
   end
 
 end
