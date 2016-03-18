7cd5f3e820eeb868daa5bfbda9fd1952ba8fe664
Ed Kerry <edwardkerry@gmail.com>
2016-03-18 12:00:30 +0000

    Adds matrix constructor - Ed and Eugene

--- edkerry/spec/canvas_spec.rb
+++ edkerry/spec/canvas_spec.rb
@@ -4,6 +4,12 @@ describe Canvas do
 
   let(:canvas) {described_class.new(4,4)}
 
+  describe'#defaults' do
+    it'should initialize with an array of dimension size' do
+      expect(canvas.dimension).to eq(Matrix[[nil,nil,nil,nil],[nil,nil,nil,nil],[nil,nil,nil,nil],[nil,nil,nil,nil]])
+    end
+  end
+
   describe '#toScreen' do
     it'should print a canvas to specified size' do
       expect(canvas.toScreen).to eq('------' + "\n" + '|    |' + "\n" + '|    |' + "\n"  + '|    |' + "\n" + '|    |' + "\n" + '------')
