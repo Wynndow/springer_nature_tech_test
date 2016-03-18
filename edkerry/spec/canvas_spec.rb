0c9c11c5cf82f3f69f856d1eea68a79d33cb63b8
Ed Kerry <edwardkerry@gmail.com>
2016-03-18 13:58:20 +0000

    Clarifies matrix cols and rows - Ed and Ed

--- edkerry/spec/canvas_spec.rb
+++ edkerry/spec/canvas_spec.rb
@@ -1,16 +1,22 @@
 describe Canvas do
 
-  let(:canvas) {described_class.new(4,4)}
+  let(:canvas) {described_class.new(4,3)}
 
   describe'#defaults' do
     it'should initialize with an array of dimension size' do
-      expect(canvas.dimension).to eq(Matrix[[nil,nil,nil,nil],[nil,nil,nil,nil],[nil,nil,nil,nil],[nil,nil,nil,nil]])
+      expect(canvas.dimension).to eq(Matrix[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]])
     end
   end
 
   describe '#toScreen' do
     it'should print a canvas to specified size' do
-      expect(canvas.toScreen).to eq('------' + "\n" + '|    |' + "\n" + '|    |' + "\n"  + '|    |' + "\n" + '|    |' + "\n" + '------')
+      expect(canvas.toScreen).to eq(
+      '-----' + "\n" +
+      '|   |' + "\n" +
+      '|   |' + "\n" +
+      '|   |' + "\n" +
+      '|   |' + "\n" +
+      '-----')
     end
   end
 end
