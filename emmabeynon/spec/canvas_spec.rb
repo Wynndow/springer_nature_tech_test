365fea9a4146402d42019beec4a4a9dc730e5ac7
emmabeynon <emma.beynon@gmail.com>
2016-03-18 11:49:38 +0000

    Added feature to display the canvas

--- emmabeynon/spec/canvas_spec.rb
+++ emmabeynon/spec/canvas_spec.rb
@@ -1,15 +1,26 @@
 require 'canvas'
 
 describe Canvas do
-  subject(:canvas) { described_class.new(20, 4)}
+  subject(:canvas) { described_class.new(10, 4)}
 
   describe '#default' do
     it 'initializes with a width' do
-      expect(canvas.width).to eq(20)
+      expect(canvas.width).to eq(10)
     end
 
     it 'initializes with a height' do
       expect(canvas.height).to eq(4)
+    end
+
+    it 'displays the canvas' do
+      expect(canvas.display).to eq(
+        "------------\n" +
+        "|          |\n" +
+        "|          |\n" +
+        "|          |\n" +
+        "|          |\n" +
+        "------------\n"
+      )
     end
   end
 end
--- emmabeynon/spec/features/canvas_feature_spec.rb
+++ emmabeynon/spec/features/canvas_feature_spec.rb
@@ -1,9 +1,16 @@
 require 'canvas'
 
 describe 'Canvas' do
-  subject(:canvas) { Canvas.new(20, 4)}
+  subject(:canvas) { Canvas.new(10, 4)}
+
   it 'creates a canvas with a specified width and height' do
-    expect(canvas.width).to eq(20)
-    expect(canvas.height).to eq(4)
+    expect(canvas.display).to eq(
+      "------------\n" +
+      "|          |\n" +
+      "|          |\n" +
+      "|          |\n" +
+      "|          |\n" +
+      "------------\n"
+    )
   end
 end
