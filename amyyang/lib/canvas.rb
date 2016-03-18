dc0e0759ef0494c79ef5f9f152c9014c00463deb
Amy <ayc.yang@gmail.com>
2016-03-18 14:04:29 +0000

    Refactored Drawing class & created canvas class

--- amyyang/lib/canvas.rb
+++ amyyang/lib/canvas.rb
@@ -1,3 +1,29 @@
 class Canvas
-  
+
+  attr_reader :width, :height
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+  def content
+    first_line = ""
+    (1..(width + 2)).each do
+      first_line = first_line + "-"
+    end
+    first_line += "\n"
+
+    height_lines = ""
+    (1..height).each do
+      bar = "|"
+      (1..width).each do
+        bar = bar + ' '
+      end
+      bar = bar + "|"
+      height_lines += bar + "\n"
+    end
+
+    first_line + height_lines + first_line
+  end
 end
--- amyyang/lib/drawing.rb
+++ amyyang/lib/drawing.rb
@@ -1,3 +1,5 @@
+require 'canvas'
+
 class Drawing
 
   def parse(txt_command)
@@ -6,26 +8,11 @@ class Drawing
 
   def command(txt_command)
     parameter = parse(txt_command)
+
     width = parameter[1].to_i
     height = parameter[2].to_i
 
-    first_line = ""
-    (1..(width + 2)).each do
-      first_line = first_line + "-"
-    end
-    first_line += "\n"
-
-    height_lines = ""
-    (1..height).each do
-      bar = "|"
-      (1..width).each do
-        bar = bar + ' '
-      end
-      bar = bar + "|"
-      height_lines += bar + "\n"
-    end
-
-    first_line + height_lines + first_line
+    canvas = Canvas.new(width, height)
+    canvas.content
   end
-
 end
