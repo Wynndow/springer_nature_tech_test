808010be16722ae29f94700e98d6bd2eea91319a
Ed Kerry <edwardkerry@gmail.com>
2016-03-18 11:32:42 +0000

    Adds failing test to display test canvas - Ed and Phil

--- edkerry/spec/canvas_spec.rb
+++ edkerry/spec/canvas_spec.rb
@@ -2,6 +2,11 @@ require 'canvas'
 
 describe Canvas do
 
-  
+  let(:canvas) {described_class.new(4,4)}
 
+  describe '#toScreen' do
+    it'should print a canvas to specified size' do
+      expect(canvas.toScreen).to eq('------' + "\n" + '|    |' + "\n" + '|    |' + "\n"  + '|    |' + "\n" + '|    |' + "\n" + '------')
+    end
+  end
 end
