ab4947ba3635b2144d50ce4cbafe11717b3f8b65
Andrew Dowell <adowellphoto@gmail.com>
2016-03-18 15:37:19 +0000

    attempting to get user input to play nicely

--- andydowell/js/command.js
+++ andydowell/js/command.js
@@ -1,8 +1,19 @@
-function Command() {
-  
+function Command(canvasKlass) {
+  this.canvasGenerator = canvasKlass
+//  this.readLine = require('readline')
+//
+//  this.rl = this.readLine.createInterface({
+//    input: process.stdin,
+//    output: process.stdout
+//  })
+//  this.enterCmd = this.rl.question('Enter command:', function(response) {
+//    this.parseInput(response);
+//  })
 }
 
+
 Command.prototype.newCanvas = function(width, height) {
+  this.canvas = new this.canvasGenerator(width, height);  
 }
 
 Command.prototype.parseInput = function(input) {
@@ -12,6 +23,18 @@ Command.prototype.parseInput = function(input) {
     var cmd2 = Number(cmd[2]);
     this.newCanvas(cmd1, cmd2);
   }
+  if(input[0] === 'L') {
+    var cmd = input.split(' ');
+    var x1 = Number(cmd[1]);
+    var y1 = Number(cmd[2]);
+    var x2 = Number(cmd[3]);
+    var y2 = Number(cmd[4]);
+    this.line(x1, y1, x2, y2);
+  }
+}
+
+Command.prototype.line = function(x1, y1, x2, y2) {
+  this.canvas.drawLine(x1, y1, x2, y2);
 }
 
 module.exports = Command;
--- andydowell/spec/commandSpec.js
+++ andydowell/spec/commandSpec.js
@@ -1,34 +1,26 @@
 describe('Command', function() {
   var Command = require('../js/command.js');
-  var command = new Command();
+  var Canvas = require('../js/canvas.js');
+  var command;
 
-  describe('new canvas', function() {
-    xit('should identify a new canvas command on a small 3x3 canvas', function() {
-      // based on user input 'C 20 4'
-      expect(command.newCanvas(3,3)).toEqual(
-					     '-----\n' +
-			    		     '|   |\n' +
-			    		     '|   |\n' +
-			    		     '|   |\n' +
-					     '-----\n')
-    })
-
-    xit('should identify a new canvas command and return a 20x4 canvas', function() {
-      expect(command.newCanvas(20,4)).toEqual(
-					      '----------------------\n' +
-					      '|                    |\n' +
-					      '|                    |\n' +
-					      '|                    |\n' +
-					      '|                    |\n' +
-					      '----------------------\n')
+  beforeEach(function() {
+    command = new Command(Canvas);
+  })
+  describe('on initialization', function() {
+    it('should be initialized with a canvas class generator', function() {
+      expect(command.canvasGenerator).toBeDefined();
     })
   })
-
   describe('parseInput', function() {
     it('should call a new canvas when issued with a command to do so', function() {
       spyOn(command, 'newCanvas');
       command.parseInput('C 20 4');
       expect(command.newCanvas).toHaveBeenCalledWith(20, 4);
+    })
+    it('should draw a line when issued with a command to do so', function() {
+      spyOn(command, 'line');
+      command.parseInput('L 1 2 6 2');
+      expect(command.line).toHaveBeenCalledWith(1,2,6,2);
     })
   })
 })
