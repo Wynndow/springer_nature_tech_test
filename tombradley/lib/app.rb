616192b83124b25962dee18f17d4c313159ef92a
Tom Bradley <bradley.tom.r@gmail.com>
2016-03-18 15:41:53 +0000

    Refactors canvas and starts linecommand. Needs refactor

--- tombradley/lib/app.rb
+++ tombradley/lib/app.rb
@@ -1,15 +1,36 @@
-class Draw
+class Canvas
 
-  def execute_command(command)
-    width, height = command.split[1..2].map(&:to_i)
+  def initialize
+    @canvas = nil
+  end
+
+  def reset(width, height)
+    @canvas = Array.new(height) { Array.new(width) }
+  end
+
+  def render
+    width = @canvas[0].length
     output = ''
     topbottom = (1..(width + 2)).map { '-' }.join + "\n"
     output += topbottom
-    height.times { output += '|' + (1..width).map { ' ' }.join + '|' + "\n" }
+
+    for row in @canvas
+      output += "|"
+        for cell in row
+          if cell == nil
+            cell = ' '
+          end
+          output += cell.to_s
+        end
+      output += "|\n"
+    end
     output += topbottom
   end
 end
 
+class LineCommand
+
+end
 
 
 class CommandLineDraw
@@ -25,5 +46,14 @@ class CommandLineDraw
   # while true
   #   command = request_command "enter command: "
   #   output_command(command)
+  # end
+
+  # def execute_command(command)
+  #   width, height = command.split[1..2].map(&:to_i)
+  #   output = ''
+  #   topbottom = (1..(width + 2)).map { '-' }.join + "\n"
+  #   output += topbottom
+  #   height.times { output += '|' + (1..width).map { ' ' }.join + '|' + "\n" }
+  #   output += topbottom
   # end
 end
--- tombradley/spec/app_spec.rb
+++ tombradley/spec/app_spec.rb
@@ -1,12 +1,12 @@
 require 'app'
 
-describe Draw do
-  subject(:draw) { described_class.new }
+describe Canvas do
+  subject(:canvas) { described_class.new }
 
   describe '#execute_command' do
     it 'draws a canvas of the correct dimensions' do
-      input = draw.execute_command('C 4 2')
-      expect(input).to eq(%q(------
+      canvas.reset(4, 2)
+      expect(canvas.render).to eq(%q(------
 |    |
 |    |
 ------
@@ -14,8 +14,29 @@ describe Draw do
     end
 
     it 'draws a different canvas correctly' do
-      input = draw.execute_command('C 1 1')
-      expect(input).to eq("---\n" + "| |\n" + "---\n")
+      canvas.reset(1, 1)
+      expect(canvas.render).to eq("---\n" + "| |\n" + "---\n")
+    end
+  end
+end
+
+
+xdescribe LineCommand do
+  describe '#draw_line' do
+    it 'draws a horizontal line of the correct dimensions' do
+      command = LineCommand.new(1, 2, 6, 2)
+
+      canvas = Canvas.new
+      canvas.reset(20, 4)
+
+      command.execute_command(canvas)
+      expect(canvas.render()).to eq(%q(----------------------
+  |                    |
+  |xxxxxx              |
+  |                    |
+  |                    |
+  ----------------------
+  ))
     end
   end
 end
