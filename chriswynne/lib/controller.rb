adda86b2b62980544497ff354b530d5f38d65b19
Chris Wynne <chris.wynndow@gmail.com>
2016-03-18 14:32:04 +0000

    Adds run method to controller

--- chriswynne/lib/controller.rb
+++ chriswynne/lib/controller.rb
@@ -1,3 +1,15 @@
 class Controller
 
+  def initialize(user_interface)
+    @user_interface = user_interface
+  end
+
+  def run
+    quit = false
+    until quit
+      command = @user_interface.get_command
+      quit = (command == "Q")
+    end
+  end
+
 end
--- chriswynne/spec/controller_spec.rb
+++ chriswynne/spec/controller_spec.rb
@@ -2,4 +2,19 @@ require 'controller.rb'
 
 describe Controller do
 
+  let(:ui) { double("ui") }
+  let(:controller) {described_class.new(ui)}
+
+  it 'can get quit from it\'s loop' do
+    allow(ui).to receive(:get_command).and_return("Q")
+    expect(ui).to receive(:get_command).once
+    controller.run
+  end
+
+  it 'continues the loop intil quit is supplied' do
+    allow(ui).to receive(:get_command).and_return("C 20 4", "Q")
+    expect(ui).to receive(:get_command).twice
+    controller.run
+  end
+
 end
