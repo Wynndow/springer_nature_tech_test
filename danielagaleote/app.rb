fd687d6de94f4fbf810f8f5bf6892282aba2d728
Daniela <daniela.galeote@gmail.com>
2016-03-18 15:36:37 +0000

    test for horizontal line on current canvas not passing

--- danielagaleote/app.rb
+++
@@ -1,8 +0,0 @@
-  require './lib/canvas'
-
-  loop do
-    puts "enter command"
-    command = gets.chomp
-    canvas = Canvas.new
-    puts canvas.userinput(command)
-  end
---
+++ danielagaleote/lib/app.rb
@@ -0,0 +1,9 @@
+  require_relative 'command'
+  require_relative 'canvas'
+
+  loop do
+    puts "enter command"
+    commandstring = gets.chomp
+    command = Command.new
+    puts command.userinput(commandstring)
+  end
