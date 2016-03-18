479e203bb3c6cd72dd5375478cf950e74a756f28
Andrew Dowell <adowellphoto@gmail.com>
2016-03-18 13:57:25 +0000

    can draw both vertical and horizontal lines

--- andydowell/js/canvas.js
+++ andydowell/js/canvas.js
@@ -16,16 +16,20 @@ Canvas.prototype.generateCanvas = function() {
 }
 
 Canvas.prototype.drawPoint = function(x, y) {
+  if(x >= this.width || x < 0 || y >= this.height || y < 0) {
+    throw "Canvas is " + this.height + "x" + this.width + "; Unable to draw point x:" + x + " y:" + y + " outside of canvas"
+  }
   this.content[y][x] = 'x';
 }
 
 Canvas.prototype.drawLine = function(x1, y1, x2, y2) {
-  for(var row = 0; row < this.content.length; row++) {
-    
-    for(var col = 0; col < this.content[0].length; col++) {
-      if(x1 <= col && col <= x2) {
-	this.drawPoint(col, row);
-      } 
+  if(y1 === y2) {
+    for(var col = x1; col <= x2; col++) {
+      this.drawPoint(col, y1); 
+    }
+  } else {
+    for(var row = y1; row <= y2; row++) {
+      this.drawPoint(x1, row);
     }
   }
 }
--- andydowell/spec/canvasSpec.js
+++ andydowell/spec/canvasSpec.js
@@ -24,6 +24,11 @@ describe('canvas', function() {
       canvas.drawPoint(2,3);
       expect(canvas.content[3][2]).toEqual('x');
     })
+    it('shouldnt be able to draw a point outside of the canvas', function() {
+      var canvas = new Canvas(5,5);
+      canvas.generateCanvas();
+      expect(function(){canvas.drawPoint(15,100)}).toThrow('Canvas is 5x5; Unable to draw point x:15 y:100 outside of canvas');
+    })
   })
 
   describe('drawLine', function() {
@@ -35,6 +40,16 @@ describe('canvas', function() {
       expect(canvas.content[0][1]).toEqual('x');
       expect(canvas.content[0][2]).toEqual('x');
       expect(canvas.content[0][3]).toEqual('x');
+      expect(canvas.content[1].indexOf('x')).toEqual(-1);
+    })
+    it('should be able to draw a vertical line between two points', function() {
+      var canvas = new Canvas(5,5);
+      canvas.generateCanvas();
+      canvas.drawLine(0,0,0,2);
+      expect(canvas.content[0][0]).toEqual('x');
+      expect(canvas.content[1][0]).toEqual('x');
+      expect(canvas.content[2][0]).toEqual('x');
+      expect(canvas.content[3][0]).toEqual(' ');
     })
   })
 })
