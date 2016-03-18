531437bd5def2c1c1d60b06c0ace4f450854aaf3
Andrew Dowell <adowellphoto@gmail.com>
2016-03-18 14:29:36 +0000

    refactored tests to be more verbose

--- andydowell/spec/canvasSpec.js
+++ andydowell/spec/canvasSpec.js
@@ -1,10 +1,12 @@
 describe('canvas', function() {
   var Canvas = require('../js/canvas.js');
   var canvas; 
-  beforeEach(function() {
-    canvas = new Canvas(5,5);
-    canvas.generateCanvas();
-  })
+
+  var createNewCanvas = function(width, height) {
+    var tempCanvas = new Canvas(width, height);
+    tempCanvas.generateCanvas();
+    return tempCanvas;
+  }
   describe('on initialization', function() {
     it('should know it\'s dimensions', function() {
       var canvas = new Canvas(20, 4);
@@ -15,22 +17,24 @@ describe('canvas', function() {
 
   describe('generateCanvas', function() {
     it('should generate a blank canvas', function() {
-      expect(canvas.content[1][1]).toEqual(' ');
+      expect(createNewCanvas(5,5).content[1][1]).toEqual(' ');
     })
   })
 
   describe('drawPoint', function() {
     it('should be able to draw a point', function() {
+      canvas = createNewCanvas(5,5);
       canvas.drawPoint(2,3);
       expect(canvas.content[3][2]).toEqual('x');
     })
     it('shouldnt be able to draw a point outside of the canvas', function() {
-      expect(function(){canvas.drawPoint(15,100)}).toThrow('Canvas is 5x5; Unable to draw point x:15 y:100 outside of canvas');
+      expect(function(){createNewCanvas(5,5).drawPoint(15,100)}).toThrow('Canvas is 5x5; Unable to draw point x:15 y:100 outside of canvas');
     })
   })
 
   describe('drawLine', function() {
     it('should be able to draw a horizontal line between two points', function() {
+      canvas = createNewCanvas(5,5);
       canvas.drawLine(0,0,4,0);
       expect(canvas.content[0][0]).toEqual('x');
       expect(canvas.content[0][1]).toEqual('x');
@@ -39,6 +43,7 @@ describe('canvas', function() {
       expect(canvas.content[1].indexOf('x')).toEqual(-1);
     })
     it('should be able to draw a vertical line between two points', function() {
+      canvas = createNewCanvas(5,5);
       canvas.drawLine(0,0,0,2);
       expect(canvas.content[0][0]).toEqual('x');
       expect(canvas.content[1][0]).toEqual('x');
@@ -48,9 +53,10 @@ describe('canvas', function() {
   })
 
   describe('renderCanvas', function() {
-    it('should correctly render a blank canvas', function() {
+    it('should correctly render a blank square 5x5 canvas', function() {
+      canvas = createNewCanvas(5,5);
       expect(canvas.renderCanvas()).toEqual(
-					  '-------\n' +
+					  '-------\n'+
 					  '|     |\n'+
 					  '|     |\n'+
 					  '|     |\n'+
