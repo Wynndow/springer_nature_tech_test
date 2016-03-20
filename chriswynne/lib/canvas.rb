04faa29b6915218af099e22f27eca69c6d88d5c8
Chris Wynne <chris.wynndow@gmail.com>
2016-03-20 20:32:17 +0000

    Adds #draw_line to canvas

--- chriswynne/lib/canvas.rb
+++ chriswynne/lib/canvas.rb
@@ -5,13 +5,34 @@ class Canvas
   def initialize(width, height)
     @width = width
     @height = height
-    @canvas = Array.new(height, Array.new(width, ' ')) 
+    @canvas = Array.new(height, Array.new(width, ' '))
+    @canvas = Array.new(height).map{|elem| elem = Array.new(width, ' ')}
+
   end
 
   def draw
     horizontal = ("-" * (width + 2)) + "\n"
     body = @canvas.map{ |row| "|" + "#{row.join}" + "|\n" }.join
     horizontal + body + horizontal
+  end
+
+  def add_line(x1, y1, x2, y2)
+    horizontal_line(x1, y1, x2, y2) if y1 == y2
+    vertical_line(x1, y1, x2, y2) if x1 == x2
+  end
+
+  private
+
+  def horizontal_line(x1, y1, x2, y2)
+    for x_coord in (x1..x2)
+      @canvas[height - y1][x_coord-1] = 'x'
+    end
+  end
+
+  def vertical_line(x1, y1, x2, y2)
+    for y_coord in (y1..y2)
+      @canvas[y_coord][x1-1] = 'x'
+    end
   end
 
 end
--- chriswynne/spec/canvas_spec.rb
+++ chriswynne/spec/canvas_spec.rb
@@ -1,11 +1,9 @@
 require 'canvas.rb'
 
 describe Canvas do
-
-  let(:canvas) {described_class}
+  let(:canvas) { described_class }
 
   describe '.new' do
-
     it 'creates a canvas with the correct height' do
       width = 1
       height = rand(1..10)
@@ -19,35 +17,58 @@ describe Canvas do
       new_canvas = canvas.new(width, height)
       expect(new_canvas.width).to eq(width)
     end
-
   end
 
   describe '#draw' do
-
     it 'can return the correct width canvas' do
-      new_canvas = canvas.new(4,1)
+      new_canvas = canvas.new(4, 1)
+      expect(new_canvas.draw).to eq(
+        "------\n" \
+        "|    |\n" \
+        "------\n"
+      )
+    end
+
+    it 'can return the correct height canvas' do
+      new_canvas = canvas.new(1, 4)
+      expect(new_canvas.draw).to eq(
+        "---\n" \
+        "| |\n" \
+        "| |\n" \
+        "| |\n" \
+        "| |\n" \
+        "---\n"
+      )
+    end
+  end
+
+  describe '#add_line' do
+
+    it 'can add a horizontal line to the canvas' do
+      new_canvas = canvas.new(4, 4)
+      new_canvas.add_line(1, 3, 3, 3)
       expect(new_canvas.draw).to eq(
       "------\n" +
+      "|    |\n" +
+      "|xxx |\n" +
+      "|    |\n" +
       "|    |\n" +
       "------\n"
       )
     end
 
-    it 'can return the correct height canvas' do
-      new_canvas = canvas.new(1,4)
+    fit 'can add a vertical line to the canvas' do
+      new_canvas = canvas.new(4, 4)
+      new_canvas.add_line(2, 1, 2, 3)
       expect(new_canvas.draw).to eq(
-      "---\n" +
-      "| |\n" +
-      "| |\n" +
-      "| |\n" +
-      "| |\n" +
-      "---\n"
+      "------\n" +
+      "|    |\n" +
+      "| x  |\n" +
+      "| x  |\n" +
+      "| x  |\n" +
+      "------\n"
       )
     end
 
   end
-
-
-
-
 end
